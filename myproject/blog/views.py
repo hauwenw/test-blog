6300d685ab2565cba0c97aa4d4987f458a15e49e
Howard Wu <b97501012@gmail.com>
2017-10-19 16:07:56 +0800

    add requirement.txt, gitignore idea,db, stop using datetime and use timezone, add register flow

--- myproject/blog/views.py
+++ myproject/blog/views.py
@@ -4,10 +4,10 @@ from __future__ import unicode_literals
 
 from django.views.generic import DetailView, ListView
 from .models import Post, Comment
-from datetime import datetime
+from django.utils import timezone
 from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
-
+from django.contrib import messages
 
 # Create your views here.
 
@@ -17,7 +17,7 @@ class IndexView(ListView):
     template_name = 'blog/index.html'
 
     def get_queryset(self):
-        return Post.objects.filter(posted_at__lte=datetime.now()).order_by('-posted_at')
+        return Post.objects.filter(posted_at__lte=timezone.now()).order_by('-posted_at')
 
 
 class PostView(DetailView):
@@ -30,6 +30,7 @@ def post_post(request):
     if request.method == 'POST':
         new_post = Post(title=request.POST[u'title'], content=request.POST[u'content'], author=request.user)
         new_post.save()
+        messages.success(request, 'Post Successfully!')
     return redirect('index')
 
 
@@ -40,26 +41,5 @@ def post_comment(request, pk):
         post = Post.objects.get(id=pk)
         new_comment = Comment(content=content, post=post, author=request.user)
         new_comment.save()
+        messages.success(request, 'Comment Successfully!')
     return redirect('post', pk=pk)
-
-
-# def login(request):
-#
-#     if request.user.is_authenticated():
-#         return redirect('index')
-#
-#     username = request.POST.get('username', '')
-#     password = request.POST.get('password', '')
-#
-#     user = auth.authenticate(username=username, password=password)
-#
-#     if user is not None and user.is_active:
-#         auth.login(request, user)
-#         return redirect('index')
-#     else:
-#         return render(request, 'login.html')
-#
-#
-# def logout(request):
-#     auth.logout(request)
-#     return redirect('index')
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -16,10 +16,12 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.contrib.auth.views import login, logout
+from views import register
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^blog/', include('blog.urls')),
+    url(r'^accounts/register/', register, name='register'),
     url(r'^accounts/login/', login, name="login"),
     url(r'^accounts/logout/', logout, name="logout"),
 ]
